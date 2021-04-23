0358e3904b86ecb997870cf97dc00f0f35ce5e08
Arkady Flitman <arflit@gmail.com>
2021-04-23 21:02:35 +0300

    fix: отправляю данные пользователя при логине

--- controllers/users.js
+++ controllers/users.js
@@ -36,7 +36,7 @@ module.exports.login = (req, res, next) => {
           httpOnly: true,
         })
         .status(200)
-        .send({ token })
+        .send(user)
         .end();
     })
     .catch(next);
