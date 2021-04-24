9d6b87c6cb8e45a57f1de6444408499d6992c247
Arkady Flitman <arflit@gmail.com>
2021-04-24 14:27:47 +0300

    fix: rewiew

--- middlewares/errors-handler.js
+++ middlewares/errors-handler.js
@@ -1,10 +1,9 @@
 function errorsHandler(err, req, res, next) {
   const { statusCode = 500, message = 'На сервере произошла ошибка' } = err;
-  console.log(err);
   res
     .status(statusCode)
     .send({
-      message,
+      message, err,
     });
 
   next();
