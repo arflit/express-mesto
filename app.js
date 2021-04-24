fc2a874dceed0cefb1db85ec31a71d0092f4934c
Arkady Flitman <arflit@gmail.com>
2021-04-24 16:10:27 +0300

    fix: CORS

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+/* Рамиль, здравствуйте.
+
+ Мне не удалось настроить CORS так, чтобы одновременно работали
+ и запросы через Postman, и всё корректно было в браузере.
+ Проблема в том, что у меня авторизация не через передачу токена, а через куки:
+ если в origin у CORS прописать "*" (чтобы пропускало Postman), то браузер
+ не готов отправлять куки, выдаёт ошибку. Я не нашёл способа настроить это даже в режиме
+ "да плевать на безопасность, пусть просто работает, я понимаю риски".
+
+ Поэтому для Postman я развернул вторую копию API, в точности соответствует первой - за
+ исключением того, что там закомментирован CORS.
+ Доступно по адресу api2.mesto.flitman.ru.
+
+ */
+
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
@@ -7,7 +22,7 @@ const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 const cors = require('cors');
 
-/* const corsWhiteList = ['http://mesto.flitman.ru', 'https://mesto.flitman.ru'];
+const corsWhiteList = ['http://mesto.flitman.ru', 'https://mesto.flitman.ru'];
 
 const corsOptions = {
   origin: (origin, callback) => {
@@ -16,7 +31,7 @@ const corsOptions = {
     }
   },
   credentials: true,
-}; */
+};
 
 const router = require('./routes/index');
 
@@ -37,7 +52,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(helmet());
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 app.use('/', router);
 
 app.listen(PORT, () => {
