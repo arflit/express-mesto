ab0cbf59a624b931592c8f86d0f4434dacf7d9df
Arkady Flitman <arflit@gmail.com>
2021-04-24 14:40:58 +0300

    fix: rewiew

--- middlewares/errors-handler.js
+++ middlewares/errors-handler.js
@@ -1,7 +1,7 @@
 function errorsHandler(err, req, res, next) {
   let { statusCode = 500, message = 'На сервере произошла ошибка' } = err;
   if (message === 'celebrate request validation failed') {
-    message = err;
+    message = err.details;
     statusCode = 400;
   }
   res
