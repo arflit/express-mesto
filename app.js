25d66cea5b663f918d64194cfaa375898dce2569
Arkady Flitman <arflit@gmail.com>
2021-04-24 15:14:43 +0300

    fix: rewiew

--- app.js
+++ app.js
@@ -5,19 +5,19 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
-/* const cors = require('cors');
+const cors = require('cors');
 
 const corsWhiteList = ['http://mesto.flitman.ru', 'https://mesto.flitman.ru'];
 
 const corsOptions = {
-  origin: (origin, callback) => {
+/*   origin: (origin, callback) => {
     if (corsWhiteList.indexOf(origin) !== -1) {
       callback(null, true);
     }
   },
-  credentials: true,
+ */  credentials: true,
 };
- */
+
 const router = require('./routes/index');
 
 const { PORT = 3000 } = process.env;
@@ -37,7 +37,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(helmet());
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 app.use('/', router);
 
 app.listen(PORT, () => {
